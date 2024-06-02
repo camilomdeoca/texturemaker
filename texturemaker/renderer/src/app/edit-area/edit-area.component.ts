6ea5d334f55e488fe97c3cf5fc769c2c4e48493d
camilomdeoca <camilomdeoca@gmail.com>
2024-06-02 01:47:15 -0300

    panning and zooming editArea

--- texturemaker/renderer/src/app/edit-area/edit-area.component.ts
+++ texturemaker/renderer/src/app/edit-area/edit-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChildren, QueryList } from '@angular/core';
+import { Component, ViewChildren, QueryList, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Vector2 } from 'vectors-typescript';
 import { CardsConnectionComponent } from '../cards-connection/cards-connection.component';
@@ -39,10 +39,20 @@ export class EditAreaComponent {
   mousePos: Vector2 = new Vector2(0, 0);
   addCardMenuData: {
     show: boolean,
-    position: Vector2 | undefined
+    position: Vector2 | undefined,
   } = {
     show: false,
     position: undefined,
+  };
+
+  panningData: {
+    editAreaPosition: Vector2,
+    startEditAreaPosition: Vector2,
+    startMousePos: Vector2 | undefined,
+  } = {
+    editAreaPosition: new Vector2(0, 0),
+    startEditAreaPosition: new Vector2(0, 0),
+    startMousePos: undefined,
   };
 
   CardType = CardType; // Expose enum to template
@@ -85,35 +95,77 @@ export class EditAreaComponent {
     console.log(this.mousePosStart);
   }
 
-  cancelStartedConnection() {
-    this.startedConnection = undefined;
-  }
-
   onMouseMove = (event: MouseEvent): void => {
     if (this.startedConnection !== undefined) {
       this.mousePos = new Vector2(event.pageX, event.pageY);
+    } else if (this.panningData.startMousePos !== undefined) {
+      this.mousePos = new Vector2(event.pageX, event.pageY);
+      this.panningData.editAreaPosition = Vector2.add(
+        this.panningData.startEditAreaPosition,
+        Vector2.subtract(
+          this.mousePos,
+          this.panningData.startMousePos
+        )
+      );
+      console.log(this.panningData.editAreaPosition);
     }
   }
 
+  @ViewChild("editor", { static: true }) editor!: ElementRef<HTMLDivElement>;
   onMouseDown(event: MouseEvent): void {
+    console.log(event.pageX, event.pageY);
     if (this.addCardMenuData.show) {
       this.addCardMenuData.show = false;
     } else if (event.button == 2) {
+      const editorRect = this.editor.nativeElement.getBoundingClientRect();
       this.addCardMenuData.position = new Vector2(
-        event.offsetX,
-        event.offsetY
+        event.pageX - editorRect.x,
+        event.pageY - editorRect.y
       );
-      console.log(this.addCardMenuData.position);
       this.addCardMenuData.show = true;
+    } else if (event.button == 0) {
+      this.panningData.startMousePos = new Vector2(event.pageX, event.pageY);
+      this.panningData.startEditAreaPosition = this.panningData.editAreaPosition;
+      console.log("pan");
     }
   }
 
+  onMouseUp(_event: MouseEvent): void {
+    this.startedConnection = undefined;
+
+    if (this.panningData.startMousePos !== undefined)
+      this.panningData.startMousePos = undefined;
+  }
+
+  onScroll(event: WheelEvent): void {
+    this.mousePos = new Vector2(event.pageX, event.pageY);
+    const editAreaRect = this.editArea.nativeElement.getBoundingClientRect();
+    const mousePosEditorAreaSpaceBefore = new Vector2(
+      (this.mousePos.x - editAreaRect.x)/this.scale,
+      (this.mousePos.y - editAreaRect.y)/this.scale
+    );
+    this.scale -= 0.1*Math.sign(event.deltaY);
+    this.scale = this.scale < 0.3 ? 0.3 : this.scale > 3 ? 3 : this.scale;
+    const mousePosEditorAreaSpaceAfter = new Vector2(
+      (this.mousePos.x - editAreaRect.x)/this.scale,
+      (this.mousePos.y - editAreaRect.y)/this.scale
+    );
+    const deltaPosPageSpace = Vector2.subtract(mousePosEditorAreaSpaceAfter, mousePosEditorAreaSpaceBefore).times(this.scale);
+    this.panningData.editAreaPosition = Vector2.add(this.panningData.editAreaPosition, deltaPosPageSpace);
+    console.log(this.panningData.editAreaPosition);
+  }
+
+  @ViewChild("editArea", { static: true }) editArea!: ElementRef<HTMLDivElement>;
   onAddCardMenuSelect = (type: CardType): void => {
-    console.log("added");
+    const editorRect = this.editor.nativeElement.getBoundingClientRect();
+    const editAreaRect = this.editArea.nativeElement.getBoundingClientRect();
     this.nodes.push({
       id: this.nextValidIdInt.toString(36),
       type: type,
-      position: new Vector2(0, 0),
+      position: new Vector2(
+        (this.addCardMenuData.position!.x + editorRect.x - editAreaRect.x)/this.scale,
+        (this.addCardMenuData.position!.y + editorRect.y - editAreaRect.y)/this.scale
+      ),
     });
     this.nextValidIdInt++;
   }
@@ -136,7 +188,6 @@ export class EditAreaComponent {
       card.onChange();
       return;
     }
-    //console.log("End connection from input", input);
 
     this.connectCards({
       card: this.startedConnection.card,
@@ -161,6 +212,5 @@ export class EditAreaComponent {
 
     const node = this.nodes.splice(index, 1)[0];
     this.nodes.push(node);
-    console.log(this.nodes);
   }
 }
